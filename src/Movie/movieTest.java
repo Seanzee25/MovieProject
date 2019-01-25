6aec0bdfac9b9242e64c3098613d9e60fdd9817a
jbialon32 <35343963+jbialon32@users.noreply.github.com>
2019-01-25 17:06:22 -0600

    Added in textWriter

--- src/Movie/movieTest.java
+++ src/Movie/movieTest.java
@@ -9,11 +9,17 @@ public class movieTest {
 		ArrayList<movieObject> movieImport = textHandler.movieImporter();
 		ArrayList<movieObject> movies = movieImport;
 		
+		// Add your custom movie here and watch it output to the text file.
+		// movieObject writeTest = new movieObject(ReleaseDate, Title, Description, ReceiveDate);
+		// movies.add(writeTest);
+		
 		for (int i = 0; i < movies.size(); ++i) {
 		
 			System.out.println(movies.get(i).toString());
 		
 		}
+		
+		textHandler.textWriter(movies);
 
 	}
 	
