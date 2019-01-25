7a8712e4465357cd7ec354f6d317ecdc5d70328f
jbialon32 <35343963+jbialon32@users.noreply.github.com>
2019-01-25 03:49:39 -0600

    Update movieStatus.java
    Using Thomas's enum

--- src/Movie/movieStatus.java
+++ src/Movie/movieStatus.java
@@ -1,5 +1,8 @@
+
 package Movie;
 
 public enum movieStatus {
-
+	RELEASED, RECEIVED, UNRECEIVED
+	// enum types are for a set number of predefined constants.
+	// only two possiblities for what the status can be. 
 }
