3b353235fafe176d258d1ae64455cbe795439aed
jbialon32 <35343963+jbialon32@users.noreply.github.com>
2019-01-25 03:48:45 -0600

    Update movieObject.java
    Modified Thomas Hopkin's code to be more text import/export friendly and formated the date to show as (MM/dd/yyyy) after a movie Object's toString() call. Will add juicy comments later

--- src/Movie/movieObject.java
+++ src/Movie/movieObject.java
@@ -1,19 +1,58 @@
-package Movie;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class movieObject {
-
+	
+	private SimpleDateFormat goodDate = new SimpleDateFormat("MM/dd/yyyy");
 	private Date releaseDate;
 	private String name;
 	private String description;
 	private Date receiveDate;
+	private movieStatus status;
 	
-	movieObject(Date movieReleaseDate, String movieName, String movieDescription, Date movieReceiveDate) {
-		releaseDate = movieReleaseDate;
+	movieObject(String movieReleaseDate, String movieName, String movieDescription, String movieReceiveDate) {
+		// Constructor to define everything.
+		try {
+			releaseDate = goodDate.parse(movieReleaseDate);
+			receiveDate = goodDate.parse(movieReceiveDate);
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+			
+		}
 		name = movieName;
 		description = movieDescription;
-		receiveDate = movieReceiveDate;
+		
+		Date currDate = new Date();
+		
+		if (currDate.compareTo(receiveDate) < 0) {
+			
+			status = movieStatus.UNRECEIVED;
+			
+		}
+		
+		if (currDate.compareTo(receiveDate) >= 0 && currDate.compareTo(releaseDate) < 0) {
+			
+			status = movieStatus.RECEIVED;
+			
+		}
+		
+		if (currDate.compareTo(releaseDate) >= 0) {
+			
+			status = movieStatus.RELEASED;
+			
+		}
+		
+	}
+	
+	movieObject() {
+		// Constructor with no args.
+		releaseDate = new Date();
+		name = "Name";
+		description = "Description";
+		receiveDate = new Date();
+		this.status = movieStatus.UNRECEIVED;
 	}
 	
 	public Date getReleaseDate() {
@@ -40,5 +79,20 @@ public class movieObject {
 	public void setReceiveDate(Date receiveDate) {
 		this.receiveDate = receiveDate;
 	}
-
+	
+	public movieStatus getStatus() {
+		return status;
+	}
+	
+	public String toString() {
+		// Printing of the object in clean format.
+		String movie;
+		movie = "Release Date: " + goodDate.format(releaseDate);
+		movie += "\nName: " + name;
+		movie += "\nDescription: " + description;
+		movie += "\nRecieve Date: " + goodDate.format(receiveDate);
+		movie += "\nStatus: " + status;
+		return movie;
+	}
 }
+
