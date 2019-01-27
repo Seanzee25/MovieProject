787974d8f0a8e0e03f22fca21e6602d1077be8c2
jbialon32 <35343963+jbialon32@users.noreply.github.com>
2019-01-27 15:20:40 -0600

    Delete movieStatus.java

--- src/Movie/movieStatus.java
+++
@@ -1,8 +0,0 @@
-
-package Movie;
-
-public enum movieStatus {
-	RELEASED, RECEIVED, UNRECEIVED
-	// enum types are for a set number of predefined constants.
-	// only two possiblities for what the status can be. 
-}
